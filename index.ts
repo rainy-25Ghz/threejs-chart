83ca749545413d4485cb1f1d9f7ef23edd61545b
rainy-25Ghz <1748420480@qq.com>
2022-02-02 19:42:17 +0800

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {
   Mesh,
   MeshBasicMaterial,
 } from 'three';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 
 import './style.css';
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -17,25 +19,41 @@ const camera = new PerspectiveCamera(
   0.1,
   1000
 );
-
 const renderer = new WebGLRenderer({ canvas, antialias: true });
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+const loader = new FontLoader();
 
-camera.position.z = 5;
+loader.load('./roboto.typeface.json', function (font) {
+  const geometryText = new TextGeometry('Hello three.js!', {
+    font: font,
+    size: 80,
+    height: 5,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+  const textMesh = new Mesh(geometryText, material);
+  scene.add(textMesh);
 
-function animate() {
-  requestAnimationFrame(animate);
+  camera.position.z = 5;
+  function animate() {
+    requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  if (camera.position.z < 10 && camera.position.z > 4)
-    camera.position.z += 0.01;
-  else camera.position.z -= 0.01;
+    textMesh.rotation.x += 0.01;
+    textMesh.rotation.y += 0.01;
+    if (camera.position.z < 10 && camera.position.z > 4)
+      camera.position.z += 0.01;
+    else camera.position.z -= 0.01;
 
-  renderer.render(scene, camera);
-}
+    renderer.render(scene, camera);
+  }
 
-animate();
+  animate();
+});
+
+// const geometry = new BoxGeometry();
+
+// const cube = new Mesh(geometry, material);
